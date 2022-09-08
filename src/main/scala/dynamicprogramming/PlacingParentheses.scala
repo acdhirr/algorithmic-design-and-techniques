2134e47aa03ea3549e2bc27beee0be15ab416f70
acdhirr <acdhirr@trilobiet.nl>
2022-09-08 14:54:49 +0200

    Dynamic Programming 2

--- src/main/scala/dynamicprogramming/PlacingParentheses.scala
+++ src/main/scala/dynamicprogramming/PlacingParentheses.scala
@@ -39,10 +39,11 @@ object PlacingParentheses {
 
     /*
       (i,j) = (0,1) (0,2) (0,3) (0,4) (0,5)
-              (1,2) (1,3) (1,4) (1,5)
-              (2,3) (2,4) (2,5)
-              (3,4) (3,5)
-              (4,5)
+                    (1,2) (1,3) (1,4) (1,5)
+                          (2,3) (2,4) (2,5)
+                                (3,4) (3,5)
+                                      (4,5)
+      follow diagonals from upper left to lower right
     */
     for (s <- 1 to n; i <- 0 to n - s; j = i + s) {
 
